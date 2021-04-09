7ae42fcaea6f0f7d8522cb49457687de4346732d
Kasiro <kon.konikov@mail.ru>
2021-04-10 05:33:44 +0700

    tested function forvar

---
+++ modules/forvar.php
@@ -0,0 +1,14 @@
+<?php
+
+$settings = [
+	'use' => true
+];
+return [
+	'settings' => $settings,
+	'rules' => [
+		'/for(\s*)\((\$\w++)(.*)count\((.*)\);(.*)\) as (\$\w++) \{\n(\t*|\s*)((?:(?(R)\w++|[^}]*+\N\n)|(?R))*)\}[^}]/m' => [
+			'type' => 'string',
+			'reg' => 'for ($2$3count($4);$5){'."\n".'$7$6 = $4[$2];'."\n".'$7$8}'."\n"
+		],
+	]
+];
