1d902ed62749dfff45150d2d94411cacb7d5b09a
Kasiro <kon.konikov@mail.ru>
2021-05-12 00:46:45 +0700

    add ig

---
+++ modules/myif.php
@@ -0,0 +1,30 @@
+<?php
+
+$module = new jModule;
+$module->setSettings([
+	'use' => false
+]);
+$n = explode('.', basename(__FILE__))[0];
+$module->setName($n);
+$module->addreg('/if(\s*)\((.*)\)/m', function ($matches){
+	$if_in = $matches[2];
+	echo $if_in . PHP_EOL;
+	// $res = $if_in;
+	$allstr = [];
+	$i = 1;
+	$res = preg_replace_callback('/\'(.*?[^\\\\])\'/m', function ($match) use (&$allstr, &$i){
+		$to = '%string'.($i).'_quote1%';
+		$allstr[$match[1]] = $to;
+		$i++;
+		return $to;
+	}, $if_in);
+	// $res = str_replace('main', 'res_main', $res);
+	foreach ($allstr as $to => $what){
+		$q = substr(explode('_', $what)[1], 0, -1);
+		if ($q == 'quote1') {
+			$res = str_replace($what, "'$to'", $res);
+		}
+	}
+	return 'if'.$matches[1].'('.$res.')';
+});
+return $module;
